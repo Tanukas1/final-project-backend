7b728b896867d3f68b3c17fa9ec20a83b3e3194c
m-triple-m <triplem656@gmail.com>
2021-11-30 09:05:33 +0530

    ..

--- routers/blogManager.js
+++ routers/blogManager.js
@@ -41,4 +41,28 @@ router.get("/getbyid/:id", (req, res) => {
     });
 });
 
+router.delete("/delete/:id", (req, res) => {
+  Model.findByIdAndDelete(req.params.id)
+    .then((data) => {
+      console.log("blog deleted by id");
+      res.status(200).json(data);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).json(err);
+    });
+});
+
+router.put("/update/:id", (req, res) => {
+  Model.findByIdAndUpdate(req.params.id, req.body)
+    .then((data) => {
+      console.log("portfolio deleted by id");
+      res.status(200).json(data);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).json(err);
+    });
+});
+
 module.exports = router;
